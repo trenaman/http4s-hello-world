9917f8f7f7ddec84f4aaf12ce677b6179305bad1
Adrian Trenaman <atrenaman@gilt.com>
2017-05-13 12:01:22 -0400

    Add log event on every invocation.

--- src/main/scala/com/fellurian/helloworld/HelloWorld.scala
+++ src/main/scala/com/fellurian/helloworld/HelloWorld.scala
@@ -1,15 +1,19 @@
 package com.fellurian
 
+import _root_.argonaut._
 import org.http4s._
-import org.http4s.server._
-import org.http4s.dsl._
-
-import _root_.argonaut._, Argonaut._
+import _root_.argonaut.Argonaut._
 import org.http4s.argonaut._
+import org.http4s.dsl._
+import org.log4s._
+
 
 object HelloWorld {
+  private val log = getLogger
   val service = HttpService {
-    case GET -> Root / "hello" / name =>
+    case GET -> Root / "hello" / name => {
+      log.info(s"Received 'hello' with name '${name}'")
       Ok(jSingleObject("message", jString(s"Hello, ${name}. You are a good friend.")))
+    }
   }
 }
